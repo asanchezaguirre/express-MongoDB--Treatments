714413d3a5ec494909ff4a07f0b333a801221fdf
Adriana <s.aguirre.adriana@gmail.com>
2019-01-29 12:26:26 -0600

    Sprint 2 -models

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose
+
+const userSchema = new Schema({
+	_id: Schema.Types.ObjectId,
+	name: { type: String, required: true },
+	email: { type:String, unique:true },
+});
+
+module.exports = mongoose.model('User', userSchema);
+
